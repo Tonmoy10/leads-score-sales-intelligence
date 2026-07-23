599cafc36c9beb2f41a78ee9f590f5861f35908a
Tonmoy10 <tonmoysaha1225@gmail.com>
2026-07-24 00:10:05 +0100

    initial commit: Environment setup & data ingestion.

---
+++ data_ingestion.py
@@ -0,0 +1,16 @@
+import kagglehub
+import os
+import shutil
+
+# Download latest version
+path = kagglehub.dataset_download("ashydv/leads-dataset")
+
+print("Path to dataset files:", path)
+
+os.makedirs("data",exist_ok=True)
+
+#Finding and storing the csv file name
+filename = [x for x in os.listdir(path) if x.endswith(".csv")][0]
+
+#Copying the dataset to a separate directory names "data"
+shutil.copy(src=os.path.join(path,filename),dst=os.path.join("data",filename))
