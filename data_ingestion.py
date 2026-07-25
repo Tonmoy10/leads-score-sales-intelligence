d2d9a33e28bdafde194e51f486e3a4a735a28472
Tonmoy10 <tonmoysaha1225@gmail.com>
2026-07-26 00:44:58 +0100

    refactor: Restructure project directories and update file paths.

--- data_ingestion.py
+++ data_ingestion.py
@@ -7,10 +7,10 @@ path = kagglehub.dataset_download("ashydv/leads-dataset")
 
 print("Path to dataset files:", path)
 
-os.makedirs("data",exist_ok=True)
+os.makedirs("data/raw",exist_ok=True)
 
 #Finding and storing the csv file name
 filename = [x for x in os.listdir(path) if x.endswith(".csv")][0]
 
 #Copying the dataset to a separate directory names "data"
-shutil.copy(src=os.path.join(path,filename),dst=os.path.join("data",filename))
+shutil.copy(src=os.path.join(path,filename),dst=os.path.join("data/raw",filename))
